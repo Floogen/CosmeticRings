a0e7294fc3865a52abdcd7e82842f3e8485e82d9
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-04-09 15:19:26 -0500

    Start of project

---
+++ CosmeticRings/CosmeticRings/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmeticRings
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
